13d57a86ebe3cb125fc86fc9eb1fb2f04032db40
SATHEESH <sathishadipad1998@gmail.com>
2023-02-09 16:11:01 +0530

    fix

--- Tests/formData.test.tsx
+++ Tests/formData.test.tsx
@@ -1,23 +1,23 @@
 /* eslint-disable testing-library/prefer-screen-queries */
 import React from "react";
-import { render, fireEvent,waitFor ,act} from "@testing-library/react";
-import fetchMock from 'jest-fetch-mock';
-import '@testing-library/jest-dom/extend-expect';
+import { render, fireEvent, waitFor, act } from "@testing-library/react";
+import fetchMock from "jest-fetch-mock";
+import "@testing-library/jest-dom/extend-expect";
 import FormData from "../src/formdata";
 
-jest.mock('node-fetch');
+jest.mock("node-fetch");
 
 const mockData = {
   name: "3542519",
   nasa_jpl_url: "http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=3542519",
-  is_potentially_hazardous_asteroid: false
+  is_potentially_hazardous_asteroid: false,
 };
-jest.mock('console', () => ({
-  error: jest.fn()
+jest.mock("console", () => ({
+  error: jest.fn(),
 }));
 
 describe("App", () => {
-  jest.spyOn(window, 'alert').mockImplementation(() => {});
+  jest.spyOn(window, "alert").mockImplementation(() => {});
   afterEach(() => {
     jest.clearAllMocks();
   });
@@ -34,7 +34,7 @@ describe("App", () => {
   beforeEach(() => {
     global.fetch = jest.fn();
   });
-  jest.mock('console', () => {
+  jest.mock("console", () => {
     return {
       error: jest.fn(),
     };
@@ -56,67 +56,64 @@ describe("App", () => {
     expect(submitButton).not.toBeDisabled();
   });
 
-
-  it('handles form submit and updates with API data', async () => {
+  it("handles form submit and updates with API data", async () => {
     const apiData = {
       name: "(2010 PK9)",
       nasa_jpl_url: "http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=3542519",
-      neo_reference_id: "3542519"
+      neo_reference_id: "3542519",
     };
 
     global.fetch = jest.fn().mockResolvedValue({
-      json: () => Promise.resolve(apiData)
+      json: () => Promise.resolve(apiData),
     });
 
     const { getByText, getByPlaceholderText } = render(<FormData />);
-    const textInput = getByPlaceholderText('Enter Asteroid ID');
-    const submitButton = getByText('Submit');
+    const textInput = getByPlaceholderText("Enter Asteroid ID");
+    const submitButton = getByText("Submit");
 
-    fireEvent.change(textInput, { target: { value: '283728' } });
+    fireEvent.change(textInput, { target: { value: "283728" } });
     fireEvent.submit(submitButton);
 
     await waitFor(() => {
       expect(getByText(`Name: ${apiData.name}`)).toBeInTheDocument();
-    });
+    });
   });
-  
 
-  it('click on randam button and get the data', async () => {
+  it("click on randam button and get the data", async () => {
     const apiData = {
       name: "(2010 PK9)",
       nasa_jpl_url: "http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=3542519",
-      neo_reference_id: "3542519"
+      neo_reference_id: "3542519",
     };
 
     global.fetch = jest.fn().mockResolvedValue({
-      json: () => Promise.resolve(apiData)
+      json: () => Promise.resolve(apiData),
     });
 
     const { getByText } = render(<FormData />);
-    const submitButton = getByText('Generate Random');
+    const submitButton = getByText("Generate Random");
     fireEvent.submit(submitButton);
 
     await waitFor(() => {
       expect(getByText(`Name: ${apiData.name}`)).toBeInTheDocument();
-    });
+    });
   });
 
-  it('handles API errors', async () => {
-    const apiError = new Error('API error');
+  it("handles API errors", async () => {
+    const apiError = new Error("API error");
     (fetch as jest.MockedFunction<typeof fetch>).mockRejectedValue(apiError);
-    jest.spyOn(window, 'alert').mockImplementation(() => {});
+    jest.spyOn(window, "alert").mockImplementation(() => {});
 
     const { getByText, getByPlaceholderText } = render(<FormData />);
-    const textInput = getByPlaceholderText('Enter Asteroid ID');
-    const submitButton = getByText('Submit');
+    const textInput = getByPlaceholderText("Enter Asteroid ID");
+    const submitButton = getByText("Submit");
 
-    fireEvent.change(textInput, { target: { value: '283728' } });
+    fireEvent.change(textInput, { target: { value: "283728" } });
     fireEvent.submit(submitButton);
-
 
     await waitFor(() => {
       // eslint-disable-next-line testing-library/no-wait-for-multiple-assertions
       expect(window.alert).toHaveBeenCalledWith(apiError.message);
-    });
-  });
-})
+    });
+  });
+});
--- src/formdata.tsx
+++ src/formdata.tsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 interface FormData {
   text: string;
 }
 
 const FormData: React.FC = () => {
-  const [formData, setFormData] = useState<FormData>({ text: '' });
+  const [formData, setFormData] = useState<FormData>({ text: "" });
   const [apiData, setApiData] = useState<any>(null);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -15,40 +15,64 @@ const FormData: React.FC = () => {
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     try {
-      const response = await fetch(`https://api.nasa.gov/neo/rest/v1/neo/${formData.text}?api_key=UgZoAde0BqkyaV8mpp1yQPG4FIeaNSgVlvWQBgf5`);
+      const response = await fetch(
+        `https://api.nasa.gov/neo/rest/v1/neo/${formData.text}?api_key=UgZoAde0BqkyaV8mpp1yQPG4FIeaNSgVlvWQBgf5`
+      );
       const data = await response.json();
       setApiData(data);
-    } catch (error:any) {
+    } catch (error: any) {
       console.log(error);
       alert(error.message);
     }
   };
 
-  const handleRandom = async() => {
+  const handleRandom = async () => {
     // generate a random string and set it as the value of formData.text
     try {
-        const response = await fetch(`https://api.nasa.gov/neo/rest/v1/neo/3542519?api_key=UgZoAde0BqkyaV8mpp1yQPG4FIeaNSgVlvWQBgf5`);
-        const data = await response.json();
-        setApiData(data);
-      } catch (error:any) {
-        console.log(error);
-        alert(error.message);
-      }
-    };
+      const response = await fetch(
+        `https://api.nasa.gov/neo/rest/v1/neo/3542519?api_key=UgZoAde0BqkyaV8mpp1yQPG4FIeaNSgVlvWQBgf5`
+      );
+      const data = await response.json();
+      setApiData(data);
+    } catch (error: any) {
+      console.log(error);
+      alert(error.message);
+    }
+  };
 
   return (
-    <div className="main-container" style={{ backgroundImage: `url("https://th.bing.com/th/id/OIP.R1L3kCraPi6uv7LL-VxEqwHaGA?pid=ImgDet&rs=1/150x150")` }}>
+    <div
+      className="main-container"
+      style={{
+        backgroundImage: `url("https://th.bing.com/th/id/OIP.R1L3kCraPi6uv7LL-VxEqwHaGA?pid=ImgDet&rs=1/150x150")`,
+      }}
+    >
       {apiData ? (
         <div className="details-container">
-            <p>Name: {apiData.name}</p>
-            <p>NASA JPL URL: <a href={apiData.nasa_jpl_url}>{apiData.nasa_jpl_url}</a></p>
-            <p>Is Potentially Hazardous Asteroid: {apiData.is_potentially_hazardous_asteroid ? 'Yes' : 'No'}</p>
-      </div>
+          <p>Name: {apiData.name}</p>
+          <p>
+            NASA JPL URL:{" "}
+            <a href={apiData.nasa_jpl_url}>{apiData.nasa_jpl_url}</a>
+          </p>
+          <p>
+            Is Potentially Hazardous Asteroid:{" "}
+            {apiData.is_potentially_hazardous_asteroid ? "Yes" : "No"}
+          </p>
+        </div>
       ) : (
         <form onSubmit={handleSubmit} className="form-container">
-          <input type="text" value={formData.text} onChange={handleChange} placeholder="Enter Asteroid ID" />
-          <button type="submit" disabled={!formData.text}>Submit</button>
-          <button type="button" onClick={handleRandom}>Generate Random</button>
+          <input
+            type="text"
+            value={formData.text}
+            onChange={handleChange}
+            placeholder="Enter Asteroid ID"
+          />
+          <button type="submit" disabled={!formData.text}>
+            Submit
+          </button>
+          <button type="button" onClick={handleRandom}>
+            Generate Random
+          </button>
         </form>
       )}
     </div>
@@ -56,4 +80,3 @@ const FormData: React.FC = () => {
 };
 
 export default FormData;
-
