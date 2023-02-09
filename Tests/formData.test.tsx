01f5d3fab5e6de29a63cd9b38c139e17538060c7
SATHEESH <sathishadipad1998@gmail.com>
2023-02-09 10:51:46 +0530

    integrated with jest

---
+++ Tests/formData.test.tsx
@@ -0,0 +1,122 @@
+/* eslint-disable testing-library/prefer-screen-queries */
+import React from "react";
+import { render, fireEvent,waitFor ,act} from "@testing-library/react";
+import fetchMock from 'jest-fetch-mock';
+import '@testing-library/jest-dom/extend-expect';
+import Form from "../src/formdata";
+
+jest.mock('node-fetch');
+
+const mockData = {
+  name: "3542519",
+  nasa_jpl_url: "http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=3542519",
+  is_potentially_hazardous_asteroid: false
+};
+jest.mock('console', () => ({
+  error: jest.fn()
+}));
+
+describe("App", () => {
+  jest.spyOn(window, 'alert').mockImplementation(() => {});
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  beforeEach(() => {
+    fetchMock.enableMocks();
+  });
+  afterEach(() => {
+    fetchMock.resetMocks();
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  beforeEach(() => {
+    global.fetch = jest.fn();
+  });
+  jest.mock('console', () => {
+    return {
+      error: jest.fn(),
+    };
+  });
+  it("renders input and submit button", () => {
+    const { getByPlaceholderText, getByText } = render(<Form />);
+    const input = getByPlaceholderText("Enter Asteroid ID");
+    const submitButton = getByText("Submit");
+    expect(input).toBeInTheDocument();
+    expect(submitButton).toBeInTheDocument();
+  });
+
+  it("disables submit button if input is empty", () => {
+    const { getByPlaceholderText, getByText } = render(<Form />);
+    const input = getByPlaceholderText("Enter Asteroid ID");
+    const submitButton = getByText("Submit");
+    expect(submitButton).toBeDisabled();
+    fireEvent.change(input, { target: { value: "12345" } });
+    expect(submitButton).not.toBeDisabled();
+  });
+
+
+  it('handles form submit and updates with API data', async () => {
+    const apiData = {
+      name: "(2010 PK9)",
+      nasa_jpl_url: "http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=3542519",
+      neo_reference_id: "3542519"
+    };
+
+    global.fetch = jest.fn().mockResolvedValue({
+      json: () => Promise.resolve(apiData)
+    });
+
+    const { getByText, getByPlaceholderText } = render(<Form />);
+    const textInput = getByPlaceholderText('Enter Asteroid ID');
+    const submitButton = getByText('Submit');
+
+    fireEvent.change(textInput, { target: { value: '283728' } });
+    fireEvent.submit(submitButton);
+
+    await waitFor(() => {
+      expect(getByText(`Name: ${apiData.name}`)).toBeInTheDocument();
+    });
+  });
+  
+
+  it('click on randam button and get the data', async () => {
+    const apiData = {
+      name: "(2010 PK9)",
+      nasa_jpl_url: "http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=3542519",
+      neo_reference_id: "3542519"
+    };
+
+    global.fetch = jest.fn().mockResolvedValue({
+      json: () => Promise.resolve(apiData)
+    });
+
+    const { getByText } = render(<Form />);
+    const submitButton = getByText('Generate Random');
+    fireEvent.submit(submitButton);
+
+    await waitFor(() => {
+      expect(getByText(`Name: ${apiData.name}`)).toBeInTheDocument();
+    });
+  });
+
+  it('handles API errors', async () => {
+    const apiError = new Error('API error');
+    (fetch as jest.MockedFunction<typeof fetch>).mockRejectedValue(apiError);
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
+
+    const { getByText, getByPlaceholderText } = render(<Form />);
+    const textInput = getByPlaceholderText('Enter Asteroid ID');
+    const submitButton = getByText('Submit');
+
+    fireEvent.change(textInput, { target: { value: '283728' } });
+    fireEvent.submit(submitButton);
+
+
+    await waitFor(() => {
+      // eslint-disable-next-line testing-library/no-wait-for-multiple-assertions
+      expect(window.alert).toHaveBeenCalledWith(apiError.message);
+    });
+  });
+})
---
+++ src/formdata.tsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+interface FormData {
+  text: string;
+}
+
+const Form: React.FC = () => {
+  const [formData, setFormData] = useState<FormData>({ text: '' });
+  const [apiData, setApiData] = useState<any>(null);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, text: event.target.value });
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      const response = await fetch(`https://api.nasa.gov/neo/rest/v1/neo/${formData.text}?api_key=UgZoAde0BqkyaV8mpp1yQPG4FIeaNSgVlvWQBgf5`);
+      const data = await response.json();
+      setApiData(data);
+    } catch (error:any) {
+      console.log(error);
+      alert(error.message);
+    }
+  };
+
+  const handleRandom = async() => {
+    // generate a random string and set it as the value of formData.text
+    try {
+        const response = await fetch(`https://api.nasa.gov/neo/rest/v1/neo/3542519?api_key=UgZoAde0BqkyaV8mpp1yQPG4FIeaNSgVlvWQBgf5`);
+        const data = await response.json();
+        setApiData(data);
+      } catch (error:any) {
+        console.log(error);
+        alert(error.message);
+      }
+    };
+
+  return (
+    <div className="main-container" style={{ backgroundImage: `url("https://th.bing.com/th/id/OIP.R1L3kCraPi6uv7LL-VxEqwHaGA?pid=ImgDet&rs=1/150x150")` }}>
+      {apiData ? (
+        <div className="details-container">
+            <p>Name: {apiData.name}</p>
+            <p>NASA JPL URL: <a href={apiData.nasa_jpl_url}>{apiData.nasa_jpl_url}</a></p>
+            <p>Is Potentially Hazardous Asteroid: {apiData.is_potentially_hazardous_asteroid ? 'Yes' : 'No'}</p>
+      </div>
+      ) : (
+        <form onSubmit={handleSubmit} className="form-container">
+          <input type="text" value={formData.text} onChange={handleChange} placeholder="Enter Asteroid ID" />
+          <button type="submit" disabled={!formData.text}>Submit</button>
+          <button type="button" onClick={handleRandom}>Generate Random</button>
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default Form;
+
